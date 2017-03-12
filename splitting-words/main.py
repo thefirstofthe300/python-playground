2dbc0a23f31c04cf4cb36ba48324b89152a821a4
Daniel Seymour <dannyseeless@gmail.com>
2017-03-12 06:31:34 +0000

    Updated to support multiple words

--- splitting-words/main.py
+++ splitting-words/main.py
@@ -1,7 +1,8 @@
-
 def splitting_words(word, words):
     words_dict = {key: True for key in words}
     for x in range(len(word)):
         if words_dict.get(word[:x]) and words_dict.get(word[x:]):
             return True
+        elif words_dict.get(word[:x]):
+            return splitting_words(word[x:], words)
     return False
--- splitting-words/main_test.py
+++ splitting-words/main_test.py
@@ -2,10 +2,13 @@ import unittest
 import main
 
 class TestWordSplitting(unittest.TestCase):
-    
+
     def test_splitting_words(self):
         self.assertEqual(main.splitting_words("helloworld", ["hello", "world", "what", "is", "up"]), True)
         self.assertEqual(main.splitting_words("helloWorld", ["hello", "world", "what", "is", "up"]), False)
-        
+        self.assertEqual(main.splitting_words("helloworld", ["helloworld", "what", "is", "up"]), False)
+        self.assertEqual(main.splitting_words("helloworldis", ["hello", "world", "what", "is", "up"]), True)
+        self.assertEqual(main.splitting_words("helloworldiswhat", ["hello", "world", "what", "is", "up"]), True)
+
 if __name__ == '__main__':
     unittest.main()
