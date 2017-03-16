dcf50a85acb16bfd69dde2eb60d7b4450c6d6341
Daniel Seymour <dannyseeless@gmail.com>
2017-03-16 04:00:21 +0000

    Remove printing from splitting words

--- splitting-words/main.py
+++ splitting-words/main.py
@@ -1,8 +1,23 @@
 def splitting_words(word, words):
     words_dict = {key: True for key in words}
+    size = len(word)
+    if size == 0:
+        return False
+    indexes = [False for x in range(size + 1)]
     for x in range(len(word)):
-        if words_dict.get(word[:x]) and words_dict.get(word[x:]):
-            return True
-        elif words_dict.get(word[:x]):
-            return splitting_words(word[x:], words)
+        if indexes[x] == False and words_dict.get(word[:x]):
+            indexes[x] = True
+
+        if indexes[x] == True:
+            if x == size:
+                return True
+
+            for y in range(x, size + 1):
+                if indexes[y] == False and words_dict.get(word[x:y]):
+                    indexes[y] = True
+
+                if y == size and indexes[y] == True:
+                    return True
+
+
     return False
