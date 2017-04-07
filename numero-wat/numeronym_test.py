f172e2389108801ee537caafe61163a364465183
Daniel Seymour <dannyseeless@gmail.com>
2017-04-07 19:02:48 +0000

    numeronym_test.py

--- numero-wat/numeronym_test.py
+++ numero-wat/numeronym_test.py
@@ -4,18 +4,37 @@ import unittest
 class TestNumeronym(unittest.TestCase):
 
   def test_is_valid_numeronym(self):
-    word_list = ['kettledrums', 'keyboarders', 'keyboarding', 'keypunching',
-                 "keystroke's", "kickstand's", "kidnapper's", 'kidnappings',
-                 "kilocycle's", "kilohertz's", 'kilohertzes', "kilometer's",
-                 'kindhearted', 'kingfishers', "kinswoman's", 'kitchenette',
-                 'kitchenware', 'kleptomania', 'knackwursts', 'kneecapping',
-                 'knickknacks', 'knighthoods', 'knockwursts', "knowledge's",
-                 'knucklehead', 'kookaburras', "kookiness's"]
+    tests = [
+              [
+                "k9",
+                [
+                  'kettledrums', 'keyboarders', 'keyboarding', 'keypunching',
+                  "keystroke's", "kickstand's", "kidnapper's", 'kidnappings',
+                  "kilocycle's", "kilohertz's", 'kilohertzes', "kilometer's",
+                  'kindhearted', 'kingfishers', "kinswoman's", 'kitchenette',
+                  'kitchenware', 'kleptomania', 'knackwursts', 'kneecapping',
+                  'knickknacks', 'knighthoods', 'knockwursts', "knowledge's",
+                  'knucklehead', 'kookaburras', "kookiness's"
+                ],
+                False
+              ],
+              [
+                "k8s",
+                [
+                  'kettledrums', 'keyboarders', 'keyboarding', 'keypunching',
+                  "keystroke's", "kickstand's", "kidnapper's", 'kidnappings',
+                  "kilocycle's", "kilohertz's", 'kilohertzes', "kilometer's",
+                  'kindhearted', 'kingfishers', "kinswoman's", 'kitchenette',
+                  'kitchenware', 'kleptomania', 'knackwursts', 'kneecapping',
+                  'knickknacks', 'knighthoods', 'knockwursts', "knowledge's",
+                  'knucklehead', 'kookaburras', "kookiness's"
+                ],
+                False
+              ]
+            ]
 
-    invalid_numeronyms = ["k9", "k8s", "k8g", "k8d"]
-    valid_numeronyms = ["k7ds", "k7ks"]
+    for test in tests:
+      self.assertEqual(numeronym.is_valid_numeronym(test[0], test[1]), test[2])
 
-    for numeronym in invalid_numeronyms:
-      assertFalse(is_valid_numeronym(numeronym, word_list))
-    for numeronym in valid_numeronyms:
-      assertTrue(is_valid_numeronym(numeronym, word_list))
+if __name__ == "__main__":
+  unittest.main()
