b7824632f197f4467a61a3bbad014da063505a0f
Daniel Seymour <dannyseeless@gmail.com>
2017-04-13 01:32:33 +0000

    Betterify all the things

--- numero-wat/numeronym.py
+++ numero-wat/numeronym.py
@@ -1,5 +1,7 @@
 alphabet_str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 alphabet = {i: 1 for i in alphabet_str}
+number_str = "1234567890"
+number = {i: 1 for i in number_str}
 
 def listify_word_file(filename):
   file_obj = open(filename, 'r').read()
@@ -19,27 +21,28 @@ def parse_numeronym(numeronym):
       except:
         continue
 
-  print "parse_numeronym:", parsed_numeronym
   return parsed_numeronym
 
 def get_numeronym_length(numeronym):
   length = 0
-  for letter in numeronym:
-    if alphabet.get(letter) == 1:
+  number_pos = 0
+  for x in range(len(numeronym) - 1, -1, -1):
+    if alphabet.get(numeronym[x]) == 1:
       length += 1
-    else:
-      if type(letter) == int:
-        length += letter
+    elif number.get(numeronym[x]) == 1:
+      length += int(numeronym[x]) * (10 ** number_pos)
+      number_pos += 1
   return length
 
 
 def compare_word_to_numeronym(word, numeronym):
-  if type(numeronym[0]) != tuple:
-    numeronym = parse_numeronym(numeronym)
   for letter_pos in numeronym:
-    if letter_pos[0] == word[letter_pos[1]]:
-      continue
-    else:
+    try:
+      if letter_pos[0] == word[letter_pos[1]]:
+        continue
+      else:
+        return False
+    except IndexError:
       return False
   return True
 
@@ -56,9 +59,11 @@ Return:
 def is_valid_numeronym(numeronym, word_list):
   numeronym_len = get_numeronym_length(numeronym)
   word_short_list = [word for word in word_list if len(word) == numeronym_len and numeronym[0] == word[0]]
+  counter = 0
   parsed_numeronym = parse_numeronym(numeronym)
   for word in word_list:
-    print "is_valid_numeronym:", [word, numeronym, compare_word_to_numeronym(word, numeronym)]
-    if compare_word_to_numeronym(word, numeronym):
-      return False
+    if compare_word_to_numeronym(word, parsed_numeronym):
+      counter += 1
+      if counter > 1:
+        return False
   return True
--- numero-wat/numeronym_test.py
+++ numero-wat/numeronym_test.py
@@ -3,10 +3,47 @@ import unittest
 
 class TestNumeronym(unittest.TestCase):
 
+  def test_parse_numeronym(self):
+    tests = [
+        ['k8s', [('k', 0), ('s', 10)]],
+        ['k8', [('k', 0)]],
+        ['k9', [('k', 0)]],
+        ['ka8', [('k', 0), ('a', 1)]],
+        ['h1', [('h', 0)]]
+      ]
+
+    for test in tests:
+      self.assertSequenceEqual(numeronym.parse_numeronym(test[0]), test[1])
+
+  def test_get_numeronym_length(self):
+    tests = [
+      ("k8s", 10),
+      ("t9", 10),
+      ("k10", 11),
+      ("wa3s", 6)
+    ]
+
+    for test in tests:
+      self.assertEqual(numeronym.get_numeronym_length(test[0]), test[1])
+
+  def test_compare_word_to_numeronym(self):
+    tests = [
+        ['kubernetes', [('k', 0), ('s', 9)], True],
+        ['kats', [('k', 0)], True],
+        ['hello', [('k', 0)], False],
+        ['k', [('k', 0), ('a', 1)], False],
+        ['h1', [('h', 0)], True],
+        ['k8s', [('k', 0), ('s', 10)], False],
+        ['k1', [('h', 0)], False],
+      ]
+
+    for test in tests:
+      self.assertEqual(numeronym.compare_word_to_numeronym(test[0], test[1]), test[2])
+
   def test_is_valid_numeronym(self):
     tests = [
               [
-                "k9",
+                "k10",
                 [
                   'kettledrums', 'keyboarders', 'keyboarding', 'keypunching',
                   "keystroke's", "kickstand's", "kidnapper's", 'kidnappings',
@@ -19,7 +56,7 @@ class TestNumeronym(unittest.TestCase):
                 False
               ],
               [
-                "k8s",
+                "k8ed",
                 [
                   'kettledrums', 'keyboarders', 'keyboarding', 'keypunching',
                   "keystroke's", "kickstand's", "kidnapper's", 'kidnappings',
@@ -29,7 +66,7 @@ class TestNumeronym(unittest.TestCase):
                   'knickknacks', 'knighthoods', 'knockwursts', "knowledge's",
                   'knucklehead', 'kookaburras', "kookiness's"
                 ],
-                False
+                True
               ]
             ]
 
