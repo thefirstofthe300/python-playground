278b357d643463108dcf54b2d35e46a9d5fdc8a7
Daniel Seymour <dannyseeless@gmail.com>
2017-04-18 18:17:00 +0000

    Added skeleton and tests

---
+++ hacker-rank-week-31/beautiful_word_test.py
@@ -0,0 +1,19 @@
+import unittest
+import beautiful_word
+
+class TestBeautifulWord(unittest.TestCase):
+
+  def test_beautiful_word(self):
+
+    words = [
+      ("abacaba", "Yes"),
+      ("abba", "No"),
+      ("yes", "No"),
+      ("Hello", "No")
+    ]
+
+    for word in words:
+      self.assertTrue(beautiful_word.is_beautiful_word(word[0]), word[1])
+
+if __name__ == "__main__":
+  unittest.main()
