7a06c411ce6018864f5361430eef3b118406b1d9
Daniel Seymour <dannyseeless@gmail.com>
2017-04-19 01:19:39 +0000

    Correct constant name

--- hacker-rank-week-31/beautiful_word/beautiful_word.py
+++ hacker-rank-week-31/beautiful_word/beautiful_word.py
@@ -1,9 +1,9 @@
-vowels = {vowel: 1 for vowel in "aeiouy"}
+VOWELS = {vowel: 1 for vowel in "aeiouy"}
 
 def is_beautiful_word(word):
   for i in range(1, len(word)):
     if word[i].lower() == word[i - 1].lower():
       return "No"
-    elif vowels.get(word[i]) == 1 and vowels.get(word[i - 1]) == 1:
+    elif VOWELS.get(word[i]) == 1 and VOWELS.get(word[i - 1]) == 1:
       return "No"
   return "Yes"
