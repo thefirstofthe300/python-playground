43332cae3d5ff1a13dfa0603be50f644b5bd9525
Daniel Seymour <dannyseeless@gmail.com>
2017-05-17 06:13:10 +0000

    Test listify parameters

--- listify_word_file/listify_word_file_test.py
+++ listify_word_file/listify_word_file_test.py
@@ -6,6 +6,7 @@ class TestListifyWordFile(unittest.TestCase):
   def test_listify_word_file(self):
     with mock.patch("__builtin__.open", mock.mock_open(read_data="Hello\nworld")) as mock_open:
       mocked_list = listify_word_file.listify_word_file("hello")
+      mock_open.assert_called_with("hello", "r")
       self.assertEqual(mocked_list, ["Hello", "world"])
 
 if __name__ == "__main__":
