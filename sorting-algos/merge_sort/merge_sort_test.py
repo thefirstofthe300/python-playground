25b5fef2b5ff1dcf0f0bcd9473ef0f957bcf5a1f
Daniel Seymour <dannyseeless@gmail.com>
2017-06-13 04:43:01 +0000

    Update tests with throughput test

--- sorting-algos/merge_sort/merge_sort_test.py
+++ sorting-algos/merge_sort/merge_sort_test.py
@@ -5,15 +5,9 @@ from ddt import ddt, data, file_data, unpack
 @ddt
 class TestMergeSort(unittest.TestCase):
 
-  @data(([5, 24, 2, 3, 4, 6, 8], [2, 3, 4, 5, 6, 8, 24]),
-      ([5, 5, 5, 5, 5, 5], [5, 5, 5, 5, 5, 5]),
-      ([1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, 6, 7]),
-      ([2, 1, 4, 5, 6, 3, 4, 6, 7, 8, 10, 396, 193, 594, 29, 485, 29, 19, 1, 59, 94, 39102, 59, 289],
-       [1, 1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 10, 19, 29, 29, 59, 59, 94, 193, 289, 396, 485, 594, 39102])
-    )
-  @unpack
-  def test_merge_sort(self, unsorted_list, sorted_list):
-    self.assertEqual(merge_sort.merge_sort(unsorted_list), sorted_list)
+  @file_data('data_to_sort.json')
+  def test_merge_sort(self, test):
+    self.assertEqual(merge_sort.merge_sort(test[0]), test[1])
 
 if __name__ == '__main__':
   unittest.main()
--- sorting-algos/selection_sort/selection_sort_test.py
+++ sorting-algos/selection_sort/selection_sort_test.py
@@ -1,17 +1,13 @@
 import unittest
 import selection_sort
+from ddt import ddt, unpack, data_file, data
 
+@ddt
 class TestSelectionSort(unittest.TestCase):
 
-  def test_selection_sort(self):
-    tests = [
-      ([5,24,2,3,4,6,8], [2,3,4,5,6,8,24]),
-      ([5,5,5,5,5,5], [5,5,5,5,5,5]),
-      ([1,2,3,4,5,6,7], [1,2,3,4,5,6,7])
-    ]
-
-    for test in tests:
-      self.assertEqual(selection_sort.selection_sort(test[0]), test[1])
+  @data_file('data_to_sort.json')
+  def test_selection_sort(self, tests):
+    self.assertEqual(selection_sort.selection_sort(test[0]), test[1])
 
 if __name__ == '__main__':
   unittest.main()
